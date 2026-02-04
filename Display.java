6b44b3f7392e0d4ff0adc5791ac9374c74e1c614
Nattawat Lertchidumrongsri-6809617126 <little0747@gmail.com>
2026-02-04 16:00:11 +0000

    Add First Program Display&HelloWorld

---
+++ Display.java
@@ -0,0 +1,12 @@
+public class Display{
+    public static void main(String[] args) {
+        //คำสั่งแสดงผล
+        System.out.println("Welcome to"); //คำสั่งแสดงผลทางจอภาพ
+        System.out.println("Nattawat tutorial"); //คำสั่งแสดงผลทางจอภาพแล้วขึ้นรรทัด
+
+        /*
+        การแสดงผลออกทางจอภาพ
+        เว้นบรรทัด และ ไม่เว้นบรรทัด
+        */
+    }
+}
