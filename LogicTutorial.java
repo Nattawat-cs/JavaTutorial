a95a352d70de2af884f99d1a46fc2631bc55050a
Nattawat lertchidumrongsri <little0747@gmail.com>
2026-02-07 18:26:23 +0700

    Add LogicTutorial

---
+++ LogicTutorial.java
@@ -0,0 +1,18 @@
+public class LogicTutorial {
+    public static void main(String[] args) {
+        /*
+           AND && => และ
+           OR || => หรือ
+           NOT ! => ตรงข้าม
+
+        */
+
+       int a = 10, b = 20;
+
+       boolean c = (a==b); //true / false
+       boolean d = (a<b);
+
+       System.out.println(c && d);
+
+    }
+}
